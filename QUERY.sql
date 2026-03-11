79596fe67bed98a15465d2871c2b3071a6422c5e
BrunoMaiolo <brunosj@hotmail.it>
2026-03-11 16:43:23 +0100

    nuovo file  delle query fatte su MYSQL

---
+++ QUERY.sql
@@ -0,0 +1,32 @@
+SELECT *
+FROM students
+WHERE YEAR(date_of_birth) = 1990;
+
+SELECT *
+FROM courses
+WHERE cfu > 10;
+
+SELECT *
+FROM students
+WHERE timestampdiff(year, date_of_birth, CURDATE()) > 30;
+
+SELECT *
+FROM courses
+WHERE period = 'I semestre'
+AND year = 1;
+
+SELECT *
+FROM exams
+WHERE date = '2020-06-20'
+AND hour > '14:00:00';
+
+SELECT *
+FROM degrees
+WHERE level = 'magistrale';
+
+SELECT COUNT(*)
+FROM departments;
+
+SELECT COUNT(*)
+FROM teachers
+WHERE phone IS NULL;
