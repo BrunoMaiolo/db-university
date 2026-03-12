7a99af57ef1eb94bac9d7dd73b6dfeb0bc15c129
BrunoMaiolo <brunosj@hotmail.it>
2026-03-12 14:31:49 +0100

    2 CORSI DI LAUREA MAGISTRALE DEL DIPARTIMENTO DI NEUROSCIENZE

--- join.sql
+++ join.sql
@@ -4,3 +4,10 @@ FROM students
 JOIN degrees
 ON students.degree_id = degrees.id
 WHERE degrees.name = 'Corso di Laurea in Economia';
+
+--2 CORSI DI LAUREA MAGISTRALE DEL DIPARTIMENTO DI NEUROSCIENZE--
+SELECT degrees.*
+FROM degrees
+JOIN departments
+ON degrees.department_id = departments.id
+WHERE degrees.level = 'magistrale' AND departments.name = 'Dipartimento di Neuroscienze';
