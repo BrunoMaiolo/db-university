1187ada191a9fc1cb7d7a49be22aed865b01ccb5
BrunoMaiolo <brunosj@hotmail.it>
2026-03-12 14:37:04 +0100

    3 CORSI IN CUI INSEGNA FILVIO AMATO (ID=44)

--- join.sql
+++ join.sql
@@ -11,3 +11,10 @@ FROM degrees
 JOIN departments
 ON degrees.department_id = departments.id
 WHERE degrees.level = 'magistrale' AND departments.name = 'Dipartimento di Neuroscienze';
+
+--3 CORSI IN CUI INSEGNA FILVIO AMATO (ID=44)--
+SELECT courses.*
+FROM courses
+JOIN course_teacher
+ON courses.id = course_teacher.course_id
+WHERE course_teacher.teacher_id = 44;
