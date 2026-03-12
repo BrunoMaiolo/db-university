1f145fb7356d901ce181ab0e71cd6ea932e45324
BrunoMaiolo <brunosj@hotmail.it>
2026-03-12 15:16:21 +0100

    2 CONTARE INSEGNANTI CON UFFICIO NELLO STESSO EDIFICIO

--- group by.sql
+++ group by.sql
@@ -3,4 +3,8 @@ SELECT YEAR(enrolment_date) AS year, COUNT(*) AS num_students
 FROM students
 GROUP BY YEAR(enrolment_date);
 
+--2 CONTARE INSEGNANTI CON UFFICIO NELLO STESSO EDIFICIO--
+SELECT office_address, COUNT(*) AS num_teachers
+FROM teachers 
+GROUP BY office_address;
 
